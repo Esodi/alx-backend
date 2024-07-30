2518355216ae765a2d30cf1fde1d2fa8de185f39
Method Mkoma <gershommethod02@gmail.com>
2024-07-30 20:42:48 +0300

    task 0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' flask using module '''
+
+from flask import render_template, Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
