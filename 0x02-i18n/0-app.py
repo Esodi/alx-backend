355f96fde3ccb0d158b39e69c980c32cd1877d7d
Method Mkoma <gershommethod02@gmail.com>
2024-07-30 20:46:42 +0300

    task 0: fixing

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,8 +7,9 @@ from flask import render_template, Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes = False)
 def index():
+    ''' the index function '''
     return render_template('0-index.html')
 
 
