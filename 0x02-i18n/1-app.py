39c46a12003b85a4025a24857cea9797d322f496
Method Mkoma <gershommethod02@gmail.com>
2024-07-30 21:31:51 +0300

    task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -13,10 +13,11 @@ babel = Babel()
 class Config:
     ''' this class configs babel lang '''
     LANGUAGES = ["en", "fr"]
-    app.config['BABEL_LANGUAGE_LOCALE'] = LANGUAGES[0]
-    app.config['BABEL_TIMEZONE_LOCALE'] = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_TIMEZONE_LOCALE = 'UTC'
 
-conf = Config()
+app.config.from_object(Config)
+
 babel.init_app(app)
 
 if __name__ == "__main__":
