794acdf35ac4c7b97497354e82201c76fe2f6775
Method Mkoma <gershommethod02@gmail.com>
2024-08-23 23:25:07 +0300

    task 3

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -11,29 +11,23 @@ class LRUCache(BaseCaching):
     ''' class itself '''
     def __init__(self):
         super().__init__()
-        self.cache_data = OrderedDict()  # Use an OrderedDict to maintain the order of insertion
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
+        ''' put method '''
         if key is None or item is None:
             return
-
-        # If the key already exists, move it to the end (most recently used)
         if key in self.cache_data:
             self.cache_data.move_to_end(key)
-
         self.cache_data[key] = item
-
-        # If the number of items exceeds MAX_ITEMS, discard the least recently used item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            discarded_key, _ = self.cache_data.popitem(last=False)  # Pop the first item
+            discarded_key, _ = self.cache_data.popitem(last=False)
             print(f"DISCARD: {discarded_key}")
 
     def get(self, key):
         ''' get method '''
         if key is None or key not in self.cache_data:
             return None
-
-        # Move the key to the end to mark it as recently used
         self.cache_data.move_to_end(key)
         return self.cache_data[key]
 
